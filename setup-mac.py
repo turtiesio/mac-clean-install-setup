bbea1ce29dd0057984017440b27251181f53bb3e
David Hwang <nomad.hwang@gmail.com>
2025-07-01 13:12:25 +0900

    feat(setup-mac): 새로운 개발 도구 설치 및 앱 설치 로직 재구성
    macOS 초기 설정 스크립트에서 앱 및 도구 설치 부분을
    새로운 개발 도구 카테고리를 포함하도록 재구성했습니다.
    기존 설치 블록을 스크립트 하단으로 이동하여
    논리적인 흐름을 개선했습니다.
    
    새로 추가된 개발 도구는 watchman, Android Studio,
    Xcode이며, 이는 개발 환경 구축을 더욱 포괄적으로
    지원합니다. Visual Studio Code도 개발 도구 섹션으로
    이동하여 일관성을 높였습니다.

--- setup-mac.py
+++ setup-mac.py
@@ -27,33 +27,6 @@ utils.setup_pipx()
 utils.setup_docker_cli_colima()
 
 
-# tools
-utils.install_brew_package("mas")  # Mac App Store CLI
-utils.install_brew_package("visual-studio-code", "cask")
-utils.install_brew_package("font-d2coding", "cask")
-utils.install_brew_package("google-chrome", "cask")
-utils.install_brew_package("raycast", "cask")  # Spotlight replacement
-utils.install_brew_package("jordanbaird-ice", "cask")  # Menu bar management
-utils.install_brew_package("maccy", "cask")  # Clipboard manager
-utils.install_brew_package("aldente", "cask")  # Battery charge limiter
-utils.install_brew_package("obsidian", "cask")  # Note-taking
-utils.install_brew_package("alt-tab", "cask")  # Windows-style alt-tab
-utils.install_brew_package("keka", "cask")  # File archiver
-utils.install_brew_package("appcleaner", "cask")  # Uninstall apps completely
-utils.install_brew_package("google-drive", "cask")
-utils.install_brew_package("shottr", "cask")  # Screenshot tool
-utils.install_brew_package("kap", "cask")  # Screen recording tool
-
-
-# Mac App Store apps
-utils.install_mas_app("441258766", "Magnet")  # Window manager
-utils.install_mas_app("937984704", "Amphetamine")  # Keep Mac awake
-utils.install_mas_app("462054704", "Microsoft Word")
-utils.install_mas_app("462058435", "Microsoft Excel")
-utils.install_mas_app("462062816", "Microsoft PowerPoint")
-utils.install_mas_app("869223134", "KakaoTalk")
-
-
 utils.setup_korean_english_key_remapping()
 
 
@@ -86,3 +59,34 @@ utils.align_zsh_plugins(
 utils.setup_git_config()
 utils.setup_ssh_key()
 utils.setup_ssh_backup_cron()
+
+
+# tools
+utils.install_brew_package("mas")  # Mac App Store CLI
+utils.install_brew_package("font-d2coding", "cask")
+utils.install_brew_package("google-chrome", "cask")
+utils.install_brew_package("raycast", "cask")  # Spotlight replacement
+utils.install_brew_package("jordanbaird-ice", "cask")  # Menu bar management
+utils.install_brew_package("maccy", "cask")  # Clipboard manager
+utils.install_brew_package("aldente", "cask")  # Battery charge limiter
+utils.install_brew_package("obsidian", "cask")  # Note-taking
+utils.install_brew_package("alt-tab", "cask")  # Windows-style alt-tab
+utils.install_brew_package("keka", "cask")  # File archiver
+utils.install_brew_package("appcleaner", "cask")  # Uninstall apps completely
+utils.install_brew_package("google-drive", "cask")
+utils.install_brew_package("shottr", "cask")  # Screenshot tool
+utils.install_brew_package("kap", "cask")  # Screen recording tool
+utils.install_brew_package("watchman")  # File watcher for development tools(expo)
+
+# Development tools
+utils.install_brew_package("visual-studio-code", "cask")
+utils.install_brew_package("android-studio", "cask")  # Android development
+utils.install_mas_app("497799835", "Xcode")
+
+# Mac App Store apps
+utils.install_mas_app("441258766", "Magnet")  # Window manager
+utils.install_mas_app("937984704", "Amphetamine")  # Keep Mac awake
+utils.install_mas_app("462054704", "Microsoft Word")
+utils.install_mas_app("462058435", "Microsoft Excel")
+utils.install_mas_app("462062816", "Microsoft PowerPoint")
+utils.install_mas_app("869223134", "KakaoTalk")
